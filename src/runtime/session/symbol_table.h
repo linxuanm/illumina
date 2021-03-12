74d986331c41d90e645fa369f531f13bf9ac47d1
BuildTools <davidmaamoaix@gmail.com>
2021-03-12 20:49:51 +0800

    sym table setup

--- src/runtime/session/symbol_table.h
+++ src/runtime/session/symbol_table.h
@@ -1,6 +1,8 @@
 #ifndef ILLUMINA_SYMBOL_TABLE_H
 #define ILLUMINA_SYMBOL_TABLE_H
 
+#include "runtime/specs.h"
+
 /*
  * Each session (object code file) has a dedicated symbol table
  * for symbolic links. Entries and their corresponding links
@@ -8,10 +10,21 @@
  *
  * This is independent from the VM due to the need to dynamically
  * load an object code file.
+ *
+ * (Note that "symbolic references" here are not actually symbolic
+ * references; they merely provide a linking between opcode args
+ * and VM objects)
  */
 
-typedef struct sym_table_t {
+typedef struct sym_ref_t {
 
+    // the type of the symbolic reference
+    uint8_t tag;
+} sym_ref_t;
+
+typedef struct sym_table_t {
+    SYM_TABLE_SIZE_T size;
+    sym_ref_t sym_references;
 } sym_table_t;
 
 #endif //ILLUMINA_SYMBOL_TABLE_H
