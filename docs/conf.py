9746afb1b0d3a89ab49cc3e65c94c46fe76a6bd5
BuildTools <davidmaamoaix@gmail.com>
2021-04-27 17:53:31 +0800

    docs

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+project = 'Illumina'
+copyright = '2021, David Ma'
+author = 'David Ma'
+release = '0.1.0'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+html_theme = 'alabaster'
+html_static_path = ['_static']
