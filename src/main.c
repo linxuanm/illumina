cfb57c262c882ad984485ef8ee9df7cee9d64355
BuildTools <davidmaamoaix@gmail.com>
2021-09-10 10:05:12 +0800

    pre refractor

--- src/main.c
+++ src/main.c
@@ -21,17 +21,8 @@ void print_bits(int const size, void const * const ptr) {
 }
 
 int main() {
-    stream_t stream;
-    stream_from_file(&stream, "test.iasm");
-
-    file_rep_t *file = load_file_rep(&stream);
-
-    if (VM_THREAD_ERRNO != 0) {
-        ERROR("VM ERROR");
-        exit(1);
-    }
-
-    print_file_rep(file);
-
+    int a = -255;
+    unsigned int b = a;
+    printf("%u", b);
     return 0;
 }
