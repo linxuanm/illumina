4f7279ee79d58631386258304f2ec0dc413aa869
BuildTools <davidmaamoaix@gmail.com>
2022-11-08 22:03:42 +0800

    codemirror theme

--- app/src/assets/scripts/editor/theme.ts
+++ app/src/assets/scripts/editor/theme.ts
@@ -1,7 +1,47 @@
 import { EditorView } from '@codemirror/view';
 
 export const luxTheme = EditorView.theme({
+    "&": {
+        border: "2px solid #EAECEF",
+        fontSize: "16px",
+        backgroundColor: "#FCFCFC"
+    },
+    "&.cm-focused": {
+        outline: "none !important"
+    },
+    ".cm-scroller": {
+        lineHeight: "1.75"
+    },
     ".cm-content": {
         fontFamily: 'Fira Code'
+    },
+    ".cm-line": {
+        padding: "0 10px"
+    },
+    ".cm-gutters": {
+        color: "#8A919966",
+        backgroundColor: "#FCFCFC",
+        width: "55px",
+        borderRight: "1px solid #E9EAEB"
+    },
+    ".cm-gutter": {
+        width: "100%",
+        height: "100%"
+    },
+    ".cm-activeLineGutter": {
+        backgroundColor: "#8A91991A",
+        color: "#747A80"
+    },
+    ".cm-activeLine": {
+        backgroundColor: "#8A91991A"
+    },
+    ".cm-gutterElement": {
+        textAlign: "center !important"
+    },
+    "&.cm-focused .cm-selectionBackground, ::selection": {
+        backgroundColor: "#036DD636"
+    },
+    ".cm-cursor": {
+        borderLeft: "2px solid #FFAA33"
     }
 }, { dark: false });
